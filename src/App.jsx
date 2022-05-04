0385166330f92ca13a929093dc385b918e789414
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-04 14:11:17 -0700

    adds connect wallet interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useAddress, useMetamask } from "@thirdweb-dev/react";
+
 const App = () => {
+  const address = useAddress();
+  const connectWithMetamask = useMetamask();
+  console.log("👋 Address:", address);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to WendelDAO</h1>
+        <button onClick={connectWithMetamask} className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet connected. Welcome to WendelDAO!</h1>
     </div>
   );
 };
