ea6e5b81378d98b2b1639931247531335fadf1e6
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-04 17:04:43 -0700

    adds checkBalance useEffect to app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import { useAddress, useMetamask } from "@thirdweb-dev/react";
+import { useAddress, useMetamask, useEditionDrop } from "@thirdweb-dev/react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const address = useAddress();
   const connectWithMetamask = useMetamask();
   console.log("👋 Address:", address);
+  const editionDrop = useEditionDrop(
+    "0x235A1CbcF61b5EA255F1A5b26D6cD90448830fEB"
+  );
+  const [hasClaimedNFT, setHasClaimedNFT] = useState(false);
+
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+
+    const checkBalance = async () => {
+      try {
+        const balance = await editionDrop.balanceOf(address, 0);
+        if (balance.gt(0)) {
+          setHasClaimedNFT(true);
+          console.log("🌟 This user has a membership NFT!");
+        } else {
+          setHasClaimedNFT(false);
+          console.log("😭 This user doesn't have a membership NFT.");
+        }
+      } catch (error) {
+        setHasClaimedNFT(false);
+        console.error("Failed to get balance", error);
+      }
+    };
+    checkBalance();
+  }, [address, editionDrop]);
 
   if (!address) {
     return (
