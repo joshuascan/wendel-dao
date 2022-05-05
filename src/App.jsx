e3f2477c2f2d8560dc7c7be3edd2c69a71a6b218
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-05 14:36:31 -0700

    adds member landing page

--- src/App.jsx
+++ src/App.jsx
@@ -62,6 +62,15 @@ const App = () => {
     );
   }
 
+  if (hasClaimedNFT) {
+    return (
+      <div className="member-page">
+        <h1>WendelDAO Member Page</h1>
+        <p>Congratulations on being a member</p>
+      </div>
+    );
+  }
+
   return (
     <div className="mint-nft">
       <h1>Mint your free DAO Membership NFT</h1>
