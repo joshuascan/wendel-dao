d1dcb477ce5c06f400872b2aadf7f7763f751f78
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-05 15:26:38 -0700

    writes deploy-token script

---
+++ scripts/5-deploy-token.js
@@ -0,0 +1,18 @@
+import { AddressZero } from "@ethersproject/constants";
+import sdk from "./1-initialize-sdk.js";
+
+(async () => {
+  try {
+    const tokenAddress = await sdk.deployer.deployToken({
+      name: "WendelDAO Governance Token",
+      symbol: "WENDEL",
+      primary_sale_recipient: AddressZero,
+    });
+    console.log(
+      "✅ Successfully deployed token module, address:",
+      tokenAddress
+    );
+  } catch (error) {
+    console.error("Failed tok deploy token module", error);
+  }
+})();
